efecffa3c3f33d75bf5a7d4b54126fc213b31ee4
João Massano <jguilherme200144@gmail.com>
2025-10-18 17:45:33 +0100

    test4

--- server.js
+++ server.js
@@ -6,10 +6,31 @@ const app = require('./app');
 
 const DB = process.env.DATABASE;
 
+/* execute: async () => {
+  let timeout = 25;
+  while (mongoose.connection.readyState === 0) {
+    if (timeout === 0) {
+      console.log('timeout');
+      throw new Error('timeout occured with mongoose connection');
+    }
+    await mongoose.connect(DB, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    timeout--;
+  }
+  console.log('Database connection status:', mongoose.connection.readyState);
+};
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log('app running on port 4000');
+}); */
+
 mongoose
   .connect(DB, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+    //useNewUrlParser: true,
+    //useUnifiedTopology: true
   })
   .then(() => {
     console.log('DB connection successful!');
