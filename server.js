a1adaec7b34287f6afbadf960688c54975929be7
João Massano <jguilherme200144@gmail.com>
2025-10-18 20:10:21 +0100

    teste15

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ async function ensureDBConnection() {
   }
 }
 
-ensureDBConnection.then(() => {
+ensureDBConnection().then(() => {
   const port = process.env.PORT || 4000;
   app.listen(port, () => {
     console.log('App running on port', port);
