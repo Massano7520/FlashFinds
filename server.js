9a58ac8089e9097ad2d095adc5c3e2c2191381bd
João Massano <jguilherme200144@gmail.com>
2025-10-18 17:25:00 +0100

    Async no inciairdo Server

--- server.js
+++ server.js
@@ -6,14 +6,39 @@ const app = require('./app');
 
 const DB = process.env.DATABASE;
 
-mongoose
-  .connect(DB, {
-    //useNewUrlParser: true,
-    //useUnifiedTopology: true
-  })
-  .then(() => console.log('DB connection succsesful!'));
+async function startServer() {
+  try {
+    await mongoose.connect(DB, {
+      //useNewUrlParser: true,
+      //useUnifiedTopology: trues
+    });
+    console.log('DB connection successful!');
+
+    const port = process.env.PORT || 4000;
+    app.listen(port, () => {
+      console.log('App running on port', port);
+    });
+  } catch (error) {
+    console.error('DB connection error:', error);
+  }
+}
+
+startServer();
+
+/*
+try {
+  await mongoose
+    .connect(DB, {
+      //useNewUrlParser: true,
+      //useUnifiedTopology: true
+    })
+   console.log('DB connection succsesful!');
+} catch (error) {
+  console.log(error);
+}
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log('app running on port 4000');
 });
+*/
